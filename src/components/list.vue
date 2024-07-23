65b977a25c3efb348a463083cdba12a76a44bb3f
bestvist <vist.sun@gmail.com>
2024-07-23 16:15:42 +0800

    feat: 添加list

---
+++ src/components/list.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="list-page">
+        <div class="card" v-for="item in list">{{ item }}</div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const list = ref([])
+onMounted(() => {
+    list = [1, 2, 3, 4];
+})
+</script>
+
+<style scoped lang="scss">
+.list {
+    padding: 40px;
+}
+</style>
